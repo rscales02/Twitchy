11dabbd63e2e021f223c5e5c79a34c52485669d4
rscales02 <rscales02@yahoo.com>
2017-09-20 19:49:04 +0200

    changes i guess...

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/freecodecamp', function(dataStream) {
-    	console.log(dataStream);
-            if (dataStream.stream === null) {
-                $("#status").html("Free Code Camp is OFFLINE");
-            } else {
-                $("#status").html("Online");
-            }
+        console.log(dataStream);
+        if (dataStream.stream === null) {
+            $("#status").html("Free Code Camp is OFFLINE");
+        } else {
+            $("#status").html("Online");
+        }
     });
 
     $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/freecodecamp', function(dataChannel) {
         console.log(dataChannel);
         $('#display-name').html(dataChannel.display_name);
-        $('#logo').html('<img src=\"' + dataChannel.logo + '\"/>');
+        $('#logo').html('<img src=\"' + dataChannel.logo + '\" class=\"center-block\"/>');
     });
 
     $.getJSON('https://wind-bow.glitch.me/twitch-api/users/freecodecamp', function(dataUser) {
-    	console.log(dataUser);
+        console.log(dataUser);
     });
 });
 /*
