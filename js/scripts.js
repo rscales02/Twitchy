01403e27f7449066e4945acfe379d22bc1cdae36
rscales02 <rscales02@yahoo.com>
2017-11-03 22:17:32 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,69 @@
 $(document).ready(function() {
+
+    //free code camp stream
     $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/freecodecamp', function(dataStream) {
-        console.log(dataStream);
+        //console.log(dataStream);
         if (dataStream.stream === null) {
-            $("#status").html("Free Code Camp is OFFLINE");
+            $("#status1").html("Free Code Camp is OFFLINE");
         } else {
-            $("#status").html("Online");
+            $("#status1").html("Online");
         }
     });
 
     $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/freecodecamp', function(dataChannel) {
-        console.log(dataChannel);
-        $('#display-name').html(dataChannel.display_name);
-        $('#logo').html('<img src=\"' + dataChannel.logo + '\" class=\"center-block\"/>');
+       // console.log(dataChannel);
+        $('#display-name1').html(dataChannel.display_name);
+        $('#logo1').html('<img src=\"' + dataChannel.logo + '\" class=\"center-block\"/>');
     });
 
     $.getJSON('https://wind-bow.glitch.me/twitch-api/users/freecodecamp', function(dataUser) {
-        console.log(dataUser);
+       // console.log(dataUser);
+       $('#description1').html(dataUser.bio);
     });
+
+    //Omgitsfirefoxx stream
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/Omgitsfirefoxx', function(dataStream) {
+       // console.log(dataStream);
+        if (dataStream.stream === null) {
+            $("#status2").html("Free Code Camp is OFFLINE");
+        } else {
+            $("#status2").html("Online");
+        }
+    });
+
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/Omgitsfirefoxx', function(dataChannel) {
+        //console.log(dataChannel);
+        $('#display-name2').html(dataChannel.display_name);
+        $('#logo2').html('<img src=\"' + dataChannel.logo + '\" class=\"center-block\"/>');
+    });
+
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/users/Omgitsfirefoxx', function(dataUser) {
+        //console.log(dataUser);
+        $('#description2').html(dataUser.bio);
+    });
+
+    //kindafunnygames stream
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/streams/kindafunnygames', function(dataStream) {
+        //console.log(dataStream);
+        if (dataStream.stream === null) {
+            $("#status3").html("Free Code Camp is OFFLINE");
+        } else {
+            $("#status3").html("Online");
+        }
+    });
+
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/channels/kindafunnygames', function(dataChannel) {
+        //console.log(dataChannel);
+        $('#display-name3').html(dataChannel.display_name);
+        $('#logo3').html('<img src=\"' + dataChannel.logo + '\" class=\"center-block\"/>');
+    });
+
+    $.getJSON('https://wind-bow.glitch.me/twitch-api/users/kindafunnygames', function(dataUser) {
+        console.log(dataUser);
+        $('#description3').html(dataUser.bio);
+    });
+
+
 });
 /*
 https://wind-bow.glitch.me/twitch-api/channels/freecodecamp
